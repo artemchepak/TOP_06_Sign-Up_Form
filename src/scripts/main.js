a3a77719d7fc2143a097e9e3902185c01f003f33
Artem Chepak <a.chepak91@gmail.com>
2024-05-30 15:06:30 -0600

    add password field verification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,33 @@
 const inputs = document.querySelectorAll('.form-input');
-        inputs.forEach(input => {
-            input.addEventListener('focus', function () {
-                this.parentNode.querySelector('.form-label').classList.add('focused');
-            });
-            input.addEventListener('blur', function () {
-                if (this.value === '') {
-                    this.parentNode.querySelector('.form-label').classList.remove('focused');
-                }
-            });
-            input.addEventListener('input', function () {
-                if (this.value !== '') {
-                    this.parentNode.querySelector('.form-label').classList.add('filled');
-                } else {
-                    this.parentNode.querySelector('.form-label').classList.remove('filled');
-                }
-            });
-        });
+inputs.forEach(input => {
+    input.addEventListener('focus', function () {
+        this.parentNode.querySelector('.form-label').classList.add('focused');
+    });
+    input.addEventListener('blur', function () {
+        if (this.value === '') {
+            this.parentNode.querySelector('.form-label').classList.remove('focused');
+        }
+    });
+    input.addEventListener('input', function () {
+        if (this.value !== '') {
+            this.parentNode.querySelector('.form-label').classList.add('filled');
+        } else {
+            this.parentNode.querySelector('.form-label').classList.remove('filled');
+        }
+    });
+});
+
+
+document.getElementById('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('password-confirmation').value;
+    const errorMessage = document.getElementById('error-message');
+
+    if (password !== confirmPassword) {
+        errorMessage.style.display = 'block';
+    } else {
+        errorMessage.style.display = 'none';
+    }
+});
